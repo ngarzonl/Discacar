0f276c7842affb2d486f1411047a8418c3446c96
Felipe <anfeacosta7@gmail.com>
2022-03-29 16:00:56 -0500

    Conexion a Farebase
    Eventos OnClick de MainActivity
    Toolbar funcional de  SelectOptionAuth
    Conexion y configuracion Farebase

--- app/src/main/java/com/isw/discacar/SelectOptionAuthActivity.java
+++ app/src/main/java/com/isw/discacar/SelectOptionAuthActivity.java
@@ -1,14 +1,21 @@
 package com.isw.discacar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
